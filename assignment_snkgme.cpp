02aa7f99fa1897439d39ad1af461a66802c9811b
김준엽 <th0950@kookmin.kr>
2022-05-12 17:40:52 +0900

    ...

--- assignment_snkgme.cpp
+++ assignment_snkgme.cpp
@@ -1,33 +1,60 @@
 #include <iostream>
+
 #include <conio.h>
+/*
+
+방향키 입력에 따라 방향전환을 하는 구문을 만들어 주기 위해선 conio 함수와 windows.h 가 선언되어야 한다.
+또한, getch 함수 , kbhit 함수가 사용된다.
+
+
+
+conio.h : 입력이 있었는지, 없었는지 판단하는 함수
+coni.h내에선 kbhit이라는 함수가 존재한다. 이는 버퍼에 값이 있으면 1, 없으면 0을 리턴하는데.
+한마디로 입력이 있었는지, 없었는지 판단하는 함수이다.
+getch()함수도 존재한다. 이는 버퍼에 있는 값을 꺼내오는 역할을 한다.
+kbhit ==1  -> getch()  이런 식으로 사용하면 될 것 같다.
+출처 : https://monny.tistory.com/5
+
+*/
+
+
 #include <windows.h>
-#include <time.h> // To make spawn value more random
+/*
+system("cls")와 sleep()함수를 사용하기 위한 헤더파일
+cls : 게임오버시 출력하는 것 일듯
+sleep : 지연시간 설정하는 함수
+즉, 난이도가 올라간다면, sleep함수를 타이트하게 하여, 난이도를 어렵게 해야 함
+*/
+#include <time.h> /*
+먹이 랜덤으로 스폰하기 위한 헤더파일
+*/
 using namespace std;
 bool gameOver;
-const int width = 20;
+const int width = 40;
 const int height = 20;
 int x, y, fruitX, fruitY, score;
 int tailX[100], tailY[100];
 int nTail;
-enum eDirecton { STOP = 0, LEFT, RIGHT, UP, DOWN };
-eDirecton dir;
+char recent = 'm';
+enum eDirecton { STOP = 0, LEFT, RIGHT, UP, DOWN }; // left,right,up,down 이라는 키를 정의하기 위한 구문
+eDirecton dir; // 이 구문을 dir라는 변수로 하나 만들었다.
 void Setup()
 {
     gameOver = false;
     dir = STOP;
     x = width / 2;
     y = height / 2;
-    fruitX = rand() % width;
+    fruitX = rand() % width; // X좌표가 width를 넘어버리면 안되기 때문에 % width를 해줌, 밑부분돔 ㅏ찬가지
     fruitY = rand() % height;
     score = 0;
 }
-void Draw()
+void Draw() // 입력 , 방향키쪽으로 전진하는 구문같은거 없음. 그냥 미리 판을 짜두기 위한 함수일 뿐
 {
-    system("cls"); //system("clear");
+    system("cls"); // 이 구문이 없으면, 계속계속해서 콘솔화면에 스네이크 게임 창이 생성됨
     cout << endl;
-    cout << " ";
+    cout << " "; // 맨 위에 표시되는 빈 공간
     for (int i = 0; i < width + 2; i++)
-        cout << "#";
+        cout << "-"; // 위쪽벽위쪽벽위쪽벽위쪽벽위쪽벽위쪽벽위쪽벽위쪽벽위쪽벽위쪽벽위쪽벽위쪽벽위쪽벽위쪽벽위쪽벽위쪽벽위쪽벽위쪽벽
     cout << endl;
 
     for (int i = 0; i < height; i++)
@@ -35,14 +62,14 @@ void Draw()
         for (int j = 0; j < width; j++)
         {
             if (j == 0)
-                cout << " #";
+                cout << "ㅣ"; // 왼쪽벽왼쪽벽왼쪽벽왼쪽벽왼쪽벽왼쪽벽왼쪽벽왼쪽벽왼쪽벽왼쪽벽왼쪽벽왼쪽벽왼쪽벽왼쪽벽왼쪽벽왼쪽벽
             if (i == y && j == x)
-                cout << "O";
+                cout << "O"; // 맵의 가운데에 뱀 머리 설정
             else if (i == fruitY && j == fruitX)
-                cout << "F";
+                cout << "F"; // 맵에 랜덤하게 Fruit 생성함
             else
             {
-                bool print = false;
+                bool print = false; //print라는 bool 함수 선언, 이후 이를 false로 초기화
                 for (int k = 0; k < nTail; k++)
                 {
                     if (tailX[k] == j && tailY[k] == i)
@@ -56,14 +83,14 @@ void Draw()
             }
 
             if (j == width - 1)
-                cout << "#";
+                cout << "ㅣ"; // 오른쪽벽오른쪽벽오른쪽벽오른쪽벽오른쪽벽오른쪽벽오른쪽벽오른쪽벽오른쪽벽오른쪽벽오른쪽벽오른쪽벽오른쪽벽
         }
         cout << endl;
     }
 
     cout << " ";
     for (int i = 0; i < width + 2; i++)
-        cout << "#";
+        cout << "-"; // 아래쪽벽아래쪽벽아래쪽벽아래쪽벽아래쪽벽아래쪽벽아래쪽벽아래쪽벽아래쪽벽아래쪽벽아래쪽벽아래쪽벽아래쪽벽아래쪽벽아래쪽벽
     cout << endl;
     cout << " Score:" << score << endl;
 }
@@ -71,8 +98,52 @@ void Input()
 {
     if (_kbhit())
     {
-        switch (_getch())
+        int key = _getch();
+        if (key == 224 || key == 0) {
+            key = _getch();
+            if (key == 72 && recent != 's') {
+                dir = UP;
+                recent = 'w';
+            }
+            if (key == 75 && recent != 'd') {
+                dir = LEFT;
+                recent = 'a';
+            }
+            if (key == 77 && recent != 'a') {
+                dir = RIGHT;
+                recent = 'd';
+            }
+            if (key == 80 && recent != 'w') {
+                dir = DOWN;
+                recent = 's';
+            }
+
+
+            /*switch (key) {
+            case 72:
+                dir = UP;
+                recent = 'w';
+                break;
+            case 75 :
+                dir = LEFT;
+                recent = 'a';
+                break;
+            case 77 :
+                dir = RIGHT;
+                recent = 'd';
+                break;
+            case 80 :
+                dir = DOWN;
+                recent = 's';
+                break;
+            default:
+                break;
+            }*/
+        }
+        /*switch (_getch())
         {
+
+
         case 'a':
             dir = LEFT;
             break;
@@ -90,7 +161,7 @@ void Input()
             break;
         default:
             break;
-        }
+        }*/
     }
 }
 void Logic()
@@ -146,14 +217,14 @@ void Logic()
 }
 int main()
 {
-    system("MODE con cols=24 lines=25");
+    system("MODE con cols=50 lines=30");
     Setup();
-    while (!gameOver)
+    while (!gameOver) // 게임오버가 되지 않는 한, 다음 함수들을 차례대로 실행한다.
     {
         Draw();
         Input();
         Logic();
-        Sleep(50); //sleep(10);
+        Sleep(100); //sleep(10);
     }
     return 0;
 }
